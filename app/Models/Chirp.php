f5b714184c0e0aa62fc3316de4a85883be7f8db1
root <root@DESKTOP-HVJBO67.localdomain>
2022-10-11 22:22:16 +0900

    showing chirps is date

--- app/Models/Chirp.php
+++ app/Models/Chirp.php
@@ -2,8 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Chirp extends Model
 {
@@ -12,5 +13,10 @@ class Chirp extends Model
     protected $fillable = [
         'message',
     ];
+    
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
     
 }
