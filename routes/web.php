d68548077e94bb59a6ed55034e494731323f3382
root <root@DESKTOP-HVJBO67.localdomain>
2022-10-11 22:50:24 +0900

    Editing Chirps

--- routes/web.php
+++ routes/web.php
@@ -29,8 +29,9 @@ Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+
 Route::resource('chirps', ChirpController::class)
-    ->only(['index', 'store'])
+    ->only(['index', 'store', 'update'])
     ->middleware(['auth', 'verified']);
 
 require __DIR__.'/auth.php';
