82c99841c034a26befb76ff35fb845c7acc78b94
root <root@DESKTOP-HVJBO67.localdomain>
2022-10-11 23:04:04 +0900

    Notifications & Events

--- app/Models/Chirp.php
+++ app/Models/Chirp.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Events\ChirpCreated;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -12,6 +13,10 @@ class Chirp extends Model
 
     protected $fillable = [
         'message',
+    ];
+
+    protected $dispatchesEvents = [
+        'created' => ChirpCreated::class,
     ];
     
     public function user()
